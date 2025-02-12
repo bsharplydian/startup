e5e1a8ebde2e11d01cdbcfd29c108da7e5447a04
Jacob <jmemmott13@gmail.com>
2025-02-11 19:23:00 -0700

    add(BrowserRouter tag, import statements) to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,49 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import { Login } from './login/login';
+import { Inventory } from './inventory/inventory';
+import { Games } from './games/games';
 
 export default function App() {
     return (
-        <div className="body bg-dark text-light">
-            <header className="container-fluid">
-                <a className="title" href="#">The Bag of Holding</a>
-                <nav className="navbar navbar-dark">
-                    <div className="user-info">
-                        <a className="user" href="games.html">Current User</a>
-                        <img src="char-placeholder.png" height="20px"></img>
+        <BrowserRouter>
+            <div className="body bg-dark text-light">
+                <header className="container-fluid">
+                    <a className="title" href="#">The Bag of Holding</a>
+                    <nav className="navbar navbar-dark">
+                        <div className="user-info">
+                            <a className="user" href="games.html">Current User</a>
+                            <img src="char-placeholder.png" height="20px"></img>
+                        </div>
+                        {/* <img src="./BoH-logo.png" width="75"> --> */}
+                        <menu className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="./index.html">Login</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="./games.html">Games</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="./inventory.html">Inventory</a>
+                            </li>
+                        </menu>
+
+                    </nav>
+                </header>
+
+                <main>app components here</main>
+
+                <footer>
+                    <div className="container-fluid">
+                        <span className="text-reset">Jacob Memmott</span>
+                        <a className="text-reset" href="https://github.com/bsharplydian/startup">GitHub</a>
                     </div>
-                    {/* <img src="./BoH-logo.png" width="75"> --> */}
-                    <menu className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="./index.html">Login</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="./games.html">Games</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="./inventory.html">Inventory</a>
-                        </li>
-                    </menu>
+                </footer>
 
-                </nav>
-            </header>
-
-            <main>app components here</main>
-
-            <footer>
-                <div className="container-fluid">
-                    <span className="text-reset">Jacob Memmott</span>
-                    <a className="text-reset" href="https://github.com/bsharplydian/startup">GitHub</a>
-                </div>
-            </footer>
-
-        </div>
+            </div>
+        </BrowserRouter>
 
     );
 }
