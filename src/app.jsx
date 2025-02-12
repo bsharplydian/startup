6cfe5e21e696058b6a1898171128c6defe001bce
Jacob <jmemmott13@gmail.com>
2025-02-11 19:05:21 -0700

    add(header and footer) to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-    return <div className="body bg-dark text-light">App will display here</div>;
+    return (
+        <div className="body bg-dark text-light">
+            <header className="container-fluid">
+                <a className="title" href="#">The Bag of Holding</a>
+                <nav className="navbar navbar-dark">
+                    <div className="user-info">
+                        <a className="user" href="games.html">Current User</a>
+                        <img src="char-placeholder.png" height="20px"></img>
+                    </div>
+                    {/* <img src="./BoH-logo.png" width="75"> --> */}
+                    <menu className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="./index.html">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="./games.html">Games</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./inventory.html">Inventory</a>
+                        </li>
+                    </menu>
+
+                </nav>
+            </header>
+
+            <main>app components here</main>
+
+            <footer>
+                <div className="container-fluid">
+                    <span className="text-reset">Jacob Memmott</span>
+                    <a className="text-reset" href="https://github.com/bsharplydian/startup">GitHub</a>
+                </div>
+            </footer>
+
+        </div>
+
+    );
 }
