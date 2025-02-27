020af5a915ca813e2e783dc1243c55874284356c
Jacob <jmemmott13@gmail.com>
2025-02-27 13:36:13 -0700

    fix(sync problem), game titles now displaying properly

--- src/games/games.jsx
+++ src/games/games.jsx
@@ -11,6 +11,10 @@ export function Games(props) {
     const [games, setGames] = React.useState(getStoredGames() || {})
     const [newGameName, setNewGameName] = React.useState("");
     const [playerType, setPlayerType] = React.useState("");
+
+    // useEffect(() => {
+
+    // }, [gameIDs])
     function getStoredGames() {
         var games = {}
         Object.keys(localStorage)
@@ -20,7 +24,6 @@ export function Games(props) {
                 let game = (JSON.parse(localStorage.getItem(x)))
                 games[game.gameID] = game
             })
-        console.log(games)
         return games
     }
     async function addGame(newGameName, playerType) {
@@ -30,7 +33,7 @@ export function Games(props) {
         }
         while (gameIDs.indexOf(id) !== -1);
         var newGameIDs = [...gameIDs, id]
-        setGameIDs(newGameIDs)
+
 
         let newDm = null;
         let newPlayers = []
@@ -41,34 +44,26 @@ export function Games(props) {
         localStorage.setItem("games/" + id, JSON.stringify(game))
         localStorage.setItem("gameIDs", JSON.stringify(newGameIDs))
         setAddGameVisible(false)
+        setGames({ ...games, [id]: game })
+        setGameIDs(newGameIDs)
     }
-    // <div className="characters">
-    // <div className="character">
-    //     <Link to="/inventory"><img src="./char-placeholder.png" width="100" className="char-image"></img></Link>
-    //     <Link to="/inventory">Kaba</Link>
-    // </div>
-    // <div className="character">
-    //     <Link><img src="./add.png" width="100" className="char-image"></img></Link>
-    // </div>
-    // </div>
+
     function GameAccordion() {
-        // const [activeKey, setActiveKey] = React.useState(null);
+
         return (
             <Accordion >
                 {gameIDs.map((gameID) => {
-                    var currentGame = games[gameID] ? games[gameID] : { gameID: "-1", gameName: "loading", dm: "none", players: { "none": "none" } }
-                    var playerList = currentGame.characters
-                    console.log(gameID)
+                    var currentGame = games[gameID] ? games[gameID] : { gameID: "-1", gameName: "loading...", dm: "none", players: { "loading...": "loading..." } }
+                    var playerList = currentGame.players
+                    console.log(playerList)
                     return (
 
-                        // <GameAccordionItem gameName={currentGame.gameName} eventKey={index} key={index} charList={["wow", "wowww", "wowwww"]} />
                         <Accordion.Item eventKey={gameID.toString()} key={gameID.toString()} >
                             <Accordion.Header>
                                 {currentGame.gameName}
                                 {/* get and add player role too */}
                             </Accordion.Header>
                             <Accordion.Body>
-                                {/* <GameAccordionCharacters charList={['e', 'a', 'sports']} /> */}
                                 <div className="characters">
                                     {['e', 'a', 'sports'].map((character) => {
                                         return (
@@ -78,6 +73,9 @@ export function Games(props) {
                                             </div>
                                         )
                                     })}
+                                    <div className="character">
+                                        <Link><img src="./add.png" width="100" className="char-image"></img></Link>
+                                    </div>
                                 </div>
                             </Accordion.Body>
                         </Accordion.Item>
@@ -88,33 +86,6 @@ export function Games(props) {
         )
     }
 
-    function GameAccordionItem({ gameName, eventKey, charList }) {
-        return (
-            <Accordion.Item eventKey={toString(eventKey)}>
-                <Accordion.Header>
-                    {gameName}
-                    {/* get and add player role too */}
-                </Accordion.Header>
-                <Accordion.Body>
-                    <GameAccordionCharacters charList={charList} />
-                </Accordion.Body>
-            </Accordion.Item>
-        )
-    }
-    function GameAccordionCharacters({ charList }) {
-        return (
-            <div className="characters">
-                {charList.map((character) => {
-                    return (
-                        <div className="character" key={character}>
-                            <Link to="/inventory"><img src="./char-placeholder.png" width="100" className="char-image"></img></Link>
-                            <Link to="/inventory">{character}</Link>
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
     return (
         <main className="games-main">
             <h1>Games</h1>
