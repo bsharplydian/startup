c12dd57490a48ee41e6e2a7b783747c90cb6d18f
Jacob <jmemmott13@gmail.com>
2025-03-12 18:50:00 -0600

    add(stubs for endpoint helper functions)

--- service/index.js
+++ service/index.js
@@ -4,9 +4,9 @@ const bcrypt = require('bcryptjs')
 const uuid = require('uuid')
 const app = express();
 
-let users = []
-let games = []
-let inventories = []
+let users = [] // user: {username, password, authToken}
+let games = [] // game: {gameID, gameName, dm, players[]} // player: {id, characterName, username}
+let inventories = [] // inventory: {equipment[]}
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -23,20 +23,20 @@ apiRouter.post("/auth/login", async (req, res) => {
     if (user) {
         if (await bcrypt.compare(req.body.password, user.password)) {
             user.token = uuid.v4();
-            res.cookie("authToken", user.token, { secure: true, httpOnly: true, sameSite: 'strict' })
+            res.cookie("authToken", user.token, { secure: true, httpOnly: true, sameSite: 'strict' });
             res.send({ username: user.username })
             return
         }
     }
-    res.status(401).send({ msg: 'Username or password is incorrect' })
+    res.status(401).send({ msg: 'Username or password is incorrect' });
 });
 apiRouter.post("/auth/create", async (req, res) => {
     if (await getUser('username', req.body.username)) {
-        res.status(409).send({ msg: "User already exists" })
+        res.status(409).send({ msg: "User already exists" });
     } else {
-        const user = await createUser(req.body.username, req.body.password)
-        res.cookie("authToken", user.token, { secure: true, httpOnly: true, sameSite: 'strict' })
-        res.send({ username: user.username })
+        const user = await createUser(req.body.username, req.body.password);
+        res.cookie("authToken", user.token, { secure: true, httpOnly: true, sameSite: 'strict' });
+        res.send({ username: user.username });
     }
 });
 apiRouter.delete("/auth/logout", async (req, res) => {
@@ -47,15 +47,60 @@ apiRouter.delete("/auth/logout", async (req, res) => {
     res.clearCookie("authToken");
     res.status(204).end();
 });
-apiRouter.get("/games");
-apiRouter.post("/games");
-apiRouter.delete("/games/:id");
-apiRouter.get("/games/:id/players");
-apiRouter.post("/games/:id/players");
-apiRouter.delete("/games/:id/players/:id");
-apiRouter.get("/games/:id/players/:id/equipment-items");
-apiRouter.post("/games/:id/players/:id/equipment-items");
-apiRouter.delete("/games/:id/players/:id/equipment-items/:id");
+apiRouter.get("/games", async (req, res) => {
+    res.send(games);
+});
+apiRouter.post("/games", async (req, res) => {
+    games = addGame(req.body);
+    res.send(games);
+});
+apiRouter.delete("/games/:id", async (req, res) => {
+    games = removeGame(req.body);
+    res.send(games);
+});
+apiRouter.get("/games/:id/players", async (req, res) => {
+    res.send(games[req.params.id]["players"]);
+});
+apiRouter.post("/games/:id/players", async (req, res) => {
+    games[req.params.id]["players"] = addPlayer(req.body);
+    res.send(games[req.params.id]["players"]);
+});
+apiRouter.delete("/games/:id/players/:id", async (req, res) => {
+    games[req.params.id]["players"] = removePlayer(req.body);
+    res.send(games[req.params.id]["players"]);
+});
+apiRouter.get("/games/:gameID/players/:playerID/equipment-items", async (req, res) => {
+    res.send(inventories[req.params.gameID][req.params.playerID])
+});
+apiRouter.post("/games/:gameID/players/:playerID/equipment-items", async (req, res) => {
+    inventories[req.params.gameID][req.params.playerID] = addItem(req.body)
+    res.send(inventories[req.params.gameID][req.params.playerID])
+});
+apiRouter.delete("/games/:gameID/players/:playerID/equipment-items/:id", async (req, res) => {
+    inventories[req.params.gameID][req.params.playerID] = removeItem(req.body)
+    res.send(inventories[req.params.gameID][req.params.playerID])
+});
+
+function addGame() {
+
+}
+function removeGame() {
+
+}
+
+function addPlayer() {
+
+}
+function removePlayer() {
+
+}
+
+function addItem() {
+
+}
+function removeItem() {
+
+}
 
 async function getUser(field, value) {
     if (!value) {
